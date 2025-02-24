7d3565503cef5eb7b6aa34cc7c054ed86f71eb7c
William Alves da Silva <williamalves_silva@hotmail.com>
2025-02-24 02:10:50 -0300

    Commit

---
+++ src/main/java/com/strangler/config/GatewayConfig.java
@@ -0,0 +1,34 @@
+package com.strangler.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GatewayConfig {
+
+    // Propriedade para definir o uso do novo microserviço
+    @Value("${features.use-new-customer-service:true}")
+    private boolean useNewCustomerService;
+    
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            // Rota para o serviço "Customer"
+            .route("customer-service-route", r -> r
+                .path("/api/customers/**")
+                .filters(f -> f.stripPrefix(1))
+                .uri(useNewCustomerService ? "http://localhost:8081" : "http://legacy-system-url")
+            )
+            // Rota padrão que encaminha demais requisições ao sistema legado
+            .route("legacy-route", r -> r
+                .path("/**")
+                .uri("http://legacy-system-url")
+            )
+            .build();
+    }
+}
+
---
+++ src/test/java/com/stragler/StraglerFacedeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stragler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StraglerFacedeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
